329ddc29067510669780465a709ae3f15da6c657
Marc Roelofs <marcroelofs@macbook-pro.local>
2017-03-02 23:15:34 +0100

    Finished all logic

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -4,14 +4,7 @@
 
 // Divide all players over 3 teams. The Experienced players must be equally divided over the teams. 
 //
-// Solution:
-// - Determine the number of Experienced players (all player with "YES" and divide by 3
-// - Determine the number of total players per team; count players and divide by 3
-// - walk through each player, 
-//      check if experienced, 
-//         if yes then if number of experienced is under max and total number in team is under max: add to the team
-//         if no then is total number in team is under max: add to the team
-//         else check the same for the next team
+
 
 
 //Store players information in a Dictionary with an array [Name: [Height, Played soccer, Guardian]]
@@ -39,7 +32,26 @@ var teamSharks = [String: Array<String>]()
 var teamDragons = [String: Array<String>]()
 var teamRaptors = [String: Array<String>]()
 
+var letters = [String]()
+
+
+func calcAverageTotalHeightPerTeam() -> Int {
+    var totalHeight: Int = 0
+    
+    // Calc total average height per team
+    // Add all heights and divide by number of teams (fixed 3)
+    
+    for player in players {
+        totalHeight += Int(player.value[0])!
+ 
+    }
+    return totalHeight / 3
+}
+
 func maxNumberOfExperiencedPlayersPerTeam() -> Int {
+    
+    // Determine number of experienced players per team
+    // count all players with "YES" and divide by number of teams (fixed 3)
 
     var experiencedPlayers: Int = 0
     
@@ -52,6 +64,10 @@ func maxNumberOfExperiencedPlayersPerTeam() -> Int {
 }
 
 func isExperiencedPlayer(playerExperience: String) -> Bool {
+    
+    // Is the player an experienced player? 
+    // Return true if "YES"
+    
     if playerExperience == "YES" {
         return true
     } else {
@@ -63,57 +79,96 @@ func dividePlayers() {
     let maxNumberOfPlayersPerTeam = players.count / 3
     var numberOfPlayersTeamShark = 0
     var numberOfPlayersTeamDragons = 0
+    var numberOfPlayersTeamRaptors = 0
     var numberOfExperiencedPlayersTeamShark = 0
     var numberOfExperiencedPlayersTeamDragons = 0
+    var heightOfTeamSharks = 0
+    var heightOfTeamDragons = 0
+    var heightOfTeamRaptors = 0
     
     
     // first divide experienced players over the teams
+    
     for player in players {
         if isExperiencedPlayer(playerExperience: player.value[1]) {
             if numberOfExperiencedPlayersTeamShark < maxNumberOfExperiencedPlayersPerTeam() {
                 teamSharks[player.key] = player.value
                 numberOfExperiencedPlayersTeamShark += 1
                 numberOfPlayersTeamShark += 1
+                heightOfTeamSharks += Int(player.value[0])!
             } else if numberOfExperiencedPlayersTeamDragons < maxNumberOfExperiencedPlayersPerTeam() {
                 teamDragons[player.key] = player.value
                 numberOfExperiencedPlayersTeamDragons += 1
                 numberOfPlayersTeamDragons += 1
+                heightOfTeamDragons += Int(player.value[0])!
             } else {
                 teamRaptors[player.key] = player.value
+                numberOfPlayersTeamRaptors += 1
+                heightOfTeamRaptors += Int(player.value[0])!
             }
         }
     }
     
-    // Then divide the others over the teams
+    // Run through the list again and divide the others over the teams (keep in mind the height of the teams)
     
     for player in players {
         if !isExperiencedPlayer(playerExperience: player.value[1]) {
-            if numberOfPlayersTeamShark < maxNumberOfPlayersPerTeam {
+            if numberOfPlayersTeamShark < maxNumberOfPlayersPerTeam && heightOfTeamSharks <= calcAverageTotalHeightPerTeam() {
                 teamSharks[player.key] = player.value
                 numberOfExperiencedPlayersTeamShark += 1
                 numberOfPlayersTeamShark += 1
-            } else if numberOfPlayersTeamDragons < maxNumberOfPlayersPerTeam {
+                heightOfTeamSharks += Int(player.value[0])!
+            } else if numberOfPlayersTeamDragons < maxNumberOfPlayersPerTeam && heightOfTeamDragons <= calcAverageTotalHeightPerTeam(){
                 teamDragons[player.key] = player.value
                 numberOfExperiencedPlayersTeamDragons += 1
                 numberOfPlayersTeamDragons += 1
+                heightOfTeamDragons += Int(player.value[0])!
             } else {
                 teamRaptors[player.key] = player.value
+                numberOfPlayersTeamRaptors += 1
+                heightOfTeamRaptors += Int(player.value[0])!
             }
         }
     }
     
 }
 
+func createLetters(forTeam team: [String: Array<String>], teamName: String) {
+    
+    var playTime = ""
+    // Select correct play time
+    switch teamName {
+        case "Sharks": playTime = "March 17, 3pm"
+        case "Dragons": playTime = "March 17, 1pm"
+        case "Raptors": playTime = "March 18, 1pm"
+    default: print("Error: Not a valid team name")
+    }
+    
+    
+    // Create letter for each player in the team and append to all letters
+    // use \n for new line
+    
+    for player in team {
+        letters.append("Dear \(player.value[2]), \n\nThis letter is to inform you that \(player.key) has been placed in team \(teamName). \n\nThe team will play at\(playTime). \n\nCan you make sure \(player.key) arrives 15 minutes in advance of the playing time so we can start on time?\n\nYours Sincerely,\nCoach Marc")
+    }
+}
+
 
 
 
 dividePlayers()
-print("Sharks")
-print(teamSharks)
-print("Dragons")
-print(teamDragons)
-print("Raptors")
-print(teamRaptors)
+createLetters(forTeam: teamSharks, teamName: "Sharks")
+createLetters(forTeam: teamDragons, teamName: "Dragons")
+createLetters(forTeam: teamRaptors, teamName: "Raptors")
+
+for letter in letters {
+    print(letter)
+    // print a divider between the letters
+    print("\n______________________________________\n")
+}
+
+
+
 
 
 
